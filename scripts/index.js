2675bef3df319e3b50f6c0421bfcdffe3e814f8b
Assaf Isaac <aisaac1285@gmail.com>
2017-11-29 09:52:14 -0500

    adding main index.js file

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+import {render}             from 'react-dom';
+import DataActions          from 'flux/actions/DataActions.js';
+
+import Logo               from 'components/logo.js';
+import Header               from 'components/header.js';
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    Switch
+} from 'react-router-dom';
